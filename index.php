322ce28ca0f1440d2dc67604a4b72f3cfa50fa8e
Adam <jiruska.adam@seznam.cz>
2019-02-04 23:07:18 +0100

    table created

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <html>
  <head>
-  <title>PHP Test</title>
+    <title>Kosmonauti</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <meta charset="utf-8">
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+    <h1>Evidence kosmonautů</h1>
+    <table>
+        <tr>
+            <th>Jméno</th>
+            <th>Příjmení</th>
+            <th>Datum narození</th>
+            <th>Superschopnost</th>
+        </tr>
+        <tr>
+            <td>Adam</td>
+            <td>Jiruška</td>
+            <td>4.4.4444</td>
+            <td>nemá</td>
+        </tr>
+    </table>
+    <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
