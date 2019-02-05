c8864a18af37e8b5e515c8cc70ed53d89938c22e
Adam <jiruska.adam@seznam.cz>
2019-02-05 14:53:28 +0100

    database

--- database.php
+++ database.php
@@ -1,18 +1,29 @@
 <?php
     $conn = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
 
-    /*$query = 'DROP TABLE astronauts;';
+    function newAstronaut($fname, $sname, $bdate, $superpwr){
+        $query = 'INSERT INTO astronauts VALUES(DEFAULT, $fname, $sname, $bdate, $superpwr);';
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    }
+
+    function editAstronaut($fname, $sname, $bdate, $superpwr){
+        //$query = 'INSERT INTO astronauts VALUES(DEFAULT, $fname, $sname, $bdate, $superpwr);';
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    }
+
+    $query = 'DROP TABLE astronauts;';
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-    $query = 'CREATE TABLE astronauts (firstname VARCHAR(20) NOT NULL,
+    $query = 'CREATE TABLE astronauts (astroID SERIAL NOT NULL,
+                                       firstname VARCHAR(20) NOT NULL,
                                        surname VARCHAR(20) NOT NULL, 
                                        bdate DATE NOT NULL,
                                        superpower VARCHAR(50),
-                                       PRIMARY KEY(firstname, surname, bdate));';
+                                       PRIMARY KEY(astroID, firstname, surname));';
     
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());*/
 
-    $query = "INSERT INTO astronauts VALUES('Adam', 'Jiruška', '1999-01-02', 'neco');";
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    newAstronaut("David", "Janeček", 2001-05-05, "nic");
+    newAstronaut("Adam", "Jiruška", 1999-01-02, "nemá");
 
     $result = pg_query("SELECT * FROM astronauts");
     if (!$result) {
@@ -21,7 +32,7 @@
     }
 
     while ($row = pg_fetch_row($result)) {
-    echo "jméno: $row[0]  příjmení: $row[1] datum: $row[2] superschopnost: $row[3]";
+    echo "jméno: $row[1]  příjmení: $row[2] datum: $row[3] superschopnost: $row[4]";
     echo "<br />\n";
     }
 
