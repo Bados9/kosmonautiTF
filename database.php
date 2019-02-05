6526334addaf0ab239658f932e3702d410e412c3
Adam <jiruska.adam@seznam.cz>
2019-02-05 13:58:26 +0100

    database

--- database.php
+++ database.php
@@ -1,7 +1,10 @@
 <?php
     $conn = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
     echo "ahooj";
-    $query = 'CREATE TABLE astronauts (firstname, surname, bdate, superpower);';
+    $query = 'CREATE TABLE astronauts (firstname VARCHAR(20),
+                                       surname VARCHAR(20), 
+                                       bdate DATETIME,
+                                       superpower VARCHAR(50));';
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
     /*$db = parse_url(getenv("DATABASE_URL"));
