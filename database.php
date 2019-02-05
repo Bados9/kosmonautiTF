8ce08d97b094dfe7b4752ca178ac5ecb3e718644
Adam <jiruska.adam@seznam.cz>
2019-02-05 14:21:26 +0100

    database

--- database.php
+++ database.php
@@ -1,15 +1,18 @@
 <?php
     $conn = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
 
-    /*$query = 'CREATE TABLE astronauts (firstname VARCHAR(20),
-                                       surname VARCHAR(20), 
-                                       bdate DATE,
-                                       superpower VARCHAR(50));';
-    */
+    $query = 'DROP TABLE astronauts;';
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    $query = 'CREATE TABLE astronauts (firstname VARCHAR(20) NOT NULL,
+                                       surname VARCHAR(20) NOT NULL, 
+                                       bdate DATE NOT NULL,
+                                       superpower VARCHAR(50),
+                                       PRIMARY KEY(firstname, surname, bdate));';
+    
     $query = "INSERT INTO astronauts VALUES('David', 'Janeček', '2004-05-05', 'nic');";
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-    
+
 
     pg_close($dbconn);
 ?>
