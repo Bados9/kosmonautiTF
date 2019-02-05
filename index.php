5de642b40476f15aac822b9db62cd2ab3d059fcc
Adam <jiruska.adam@seznam.cz>
2019-02-05 22:25:49 +0100

    table

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
             <th>Příjmení</th>
             <th>Datum narození</th>
             <th>Superschopnost</th>
+            <th></th>
+            <th></th>
         </tr>
         <?
         $result = getAllAstronauts();
@@ -23,6 +25,8 @@
             <td> <?= $row[2]; ?></td>
             <td> <?= $row[3]; ?></td>
             <td> <?= $row[4]; ?></td>
+            <td> <button type="button" class='btn btn-default'><span class="glyphicon glyphicon-pencil text-warning"></span>Upravit</button> </td>
+            <td> <button type="button" class='btn btn-default'><span class="glyphicon glyphicon-remove text-danger"></span>Odstranit</button> </td>
         </tr>
         <?}?>
     </table>
