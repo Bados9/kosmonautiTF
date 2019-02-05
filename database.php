b7436e2676bf8a09323ca70325cc3969ea49a375
Adam <jiruska.adam@seznam.cz>
2019-02-05 13:38:54 +0100

    database

--- database.php
+++ database.php
@@ -1,6 +1,12 @@
 <?php
-    $db = parse_url(getenv("DATABASE_URL"));
+    $conn = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
+    echo "ahooj";
+    $query = 'SELECT * FROM authors';
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+    /*$db = parse_url(getenv("DATABASE_URL"));
     $db["path"] = ltrim($db["path"], "/");
     echo "ahoooj";
-    echo $db["path"];
+    echo $db["path"];*/
+    pg_close($dbconn);
 ?>
