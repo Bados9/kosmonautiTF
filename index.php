db2d7fca1e0eae55c2353c181348e0db8e38b213
Adam <jiruska.adam@seznam.cz>
2019-02-07 13:26:33 +0100

    removing

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
                                     id: removeID },
                             type: "POST",
                             success: function(){
+                                refreshTable();
                                 console.log("astronaut removed");
                             }
                 });
